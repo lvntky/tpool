11331e6e94da784d726c69ef6e0480d55774bf11
Levent Kaya <levent.kaya@codefirst.io>
2025-04-26 02:27:08 +0300

    [feature] intiial config header

---
+++ include/tpool_config.h
@@ -0,0 +1,16 @@
+/**
+ * @file tpool_config.h
+ * @author Levent Kaya (leventkayadev@gmail.com)
+ * @brief Configuration header for default/initial values of tpool components
+ * @version 0.1.0
+ * @date 2025-04-26
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifndef TPOOL_CONFIG_H_
+#define TPOOL_CONFIG_H_
+
+#define C_WORKER_SIZE 4096 
+
+#endif // TPOOL_CONFIG_H_
