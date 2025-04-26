543de52f7793e4c4fdc32ff01d1dfd9d42fee78d
Levent Kaya <levent.kaya@codefirst.io>
2025-04-26 13:13:28 +0300

    [test] test structure added

---
+++ tests/main_test.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+//#include <cmocka.h>
+
+/*
+#include "basic_test.h"
+#include "edgecases_test.h"
+#include "performance_test.h"
+*/
+
+int main(void) {
+  /*
+    const struct CMUnitTest tests[] = {
+        // Basic Tests
+        cmocka_unit_test(test_tpool_create_and_destroy),
+        cmocka_unit_test(test_tpool_submit_tasks),
+
+        // Edge Case Tests
+        cmocka_unit_test(test_tpool_zero_threads),
+        cmocka_unit_test(test_tpool_submit_null_task),
+
+        // Performance or Stress Tests
+        cmocka_unit_test(test_tpool_stress_many_tasks),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+    */
+   return 0;
+}
